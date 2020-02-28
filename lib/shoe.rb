c55e513aeb905cdfd794d6489fc304f87a2513d8
Brennan Roel <brennanroel@gmail.com>
2020-02-28 19:46:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
-class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
  
   def initialize(brand)
     @brand = brand
